7799f074e433ba731d24ab67624ca4cf9e18a826
ikefan13 <ikefan13@yahoo.com>
2021-10-01 15:36:19 -0500

    Created the Character Creator Created the Character Class

---
+++ labs/Lab2/JettSmith.CharacterCreator.ConsoleHost/Program.cs
@@ -0,0 +1,216 @@
+﻿/*
+ *  Jett Smith
+ *  ITSE 1430 - Fall 2021
+ */
+using System;
+
+namespace JettSmith.CharacterCreator.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Jett Smith");
+            Console.WriteLine("ITSE 1430 - Fall 2021");
+            Console.WriteLine("Character Creator");
+            Console.WriteLine("----------------------");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("N) New Character");
+                Console.WriteLine("V) View Character");
+                Console.WriteLine("E) Edit Character");
+                Console.WriteLine("D) Delete Character");
+                Console.WriteLine("Q) Quit");
+                Console.ResetColor();
+
+                string choice = Console.ReadLine().Trim().Substring(0, 1);
+                switch (choice.ToUpper())
+                {
+                    case "N": AddCharacter(); break;
+                    case "V": ViewCharacter(); break;
+                    case "E": EditCharacter(); break;
+                    case "D": DeleteCharacter(); break;
+                    case "Q":
+                    {
+                        if (ReadBoolean("Are you sure you wish to quit(Y/N)?")) return;
+                        break;
+                    }
+                    default: DisplayError("Please enter a valid option"); break;
+                }
+            } while (true);
+        }
+
+        static Character character;
+
+        static void AddCharacter ()
+        {
+            Character newCharacter = new Character();
+
+            do
+            {
+                newCharacter.Name = ReadString("Enter your Character's name: ", true);
+                newCharacter.Class = ChooseClass();
+                newCharacter.Race = ChooseRace();
+                newCharacter.Biography = ReadString("Enter your Character's Biography (optional):", false);
+                newCharacter.Strength = ReadInt32("Enter your Character's Strength: ", 1, 100);
+                newCharacter.Intelligence = ReadInt32("Enter your Character's Intelligence: ", 1, 100);
+                newCharacter.Agility = ReadInt32("Enter your Character's Agility: ", 1, 100);
+                newCharacter.Constitution = ReadInt32("Enter your Character's Constitution: ", 1, 100);
+                newCharacter.Charisma = ReadInt32("Enter your Character's Charisma: ", 1, 100);
+
+                string error = newCharacter.Validate();
+                if (String.IsNullOrEmpty(error))
+                {
+                    character = newCharacter;
+                    return;
+                }
+                Console.WriteLine(error);
+            } while (true);
+        }
+
+        static string ChooseClass()
+        {
+            do
+            {
+                string characterClass = ReadString("What Class is your Character\n(Cleric, Bard, Fighter, Rogue, or Wizard): ", true);
+                if (characterClass.ToUpper().Equals("CLERIC") || characterClass.ToUpper().Equals("BARD") || characterClass.ToUpper().Equals("FIGHTER") || characterClass.ToUpper().Equals("ROGUE") || characterClass.ToUpper().Equals("WIZARD"))
+                    return characterClass;
+                DisplayError("Please choose one of the Class Options");
+            } while (true);
+        }
+
+        static string ChooseRace()
+        {
+            do
+            {
+                string characterRace = ReadString("What Race is your Character\n(Dragonborn, Dwarf, Elf, Human, or Kobold): ", true);
+                if (characterRace.ToUpper().Equals("DRAGONBORN") || characterRace.ToUpper().Equals("DWARF") || characterRace.ToUpper().Equals("ELF") || characterRace.ToUpper().Equals("HUMAN") || characterRace.ToUpper().Equals("KOBOLD"))
+                    return characterRace;
+                DisplayError("Please choose one of the Race Options");
+            } while (true);
+        }
+
+        static void ViewCharacter ()
+        {
+            if (character == null)
+            {
+                DisplayError("You don't have a Character");
+                return;
+            }
+
+            Console.Clear();
+            Console.WriteLine(character.Name.PadLeft(character.Name.Length / 2 + character.Name.Length));
+            Console.WriteLine("".PadLeft(character.Name.Length * 2, '-'));
+            Console.WriteLine("Class: " + character.Class);
+            Console.WriteLine("Race: " + character.Race);
+            Console.WriteLine("Strength: " + character.Strength);
+            Console.WriteLine("Intelligence: " + character.Intelligence);
+            Console.WriteLine("Agility: " + character.Agility);
+            Console.WriteLine("Constitution: " + character.Constitution);
+            Console.WriteLine("Charisma: " + character.Charisma);
+        }
+
+        static void EditCharacter ()
+        {
+            ViewCharacter();
+            Console.WriteLine("");
+
+            if (character == null)
+            {
+                DisplayError("You don't have a Character");
+                return;
+            }
+
+            do
+            {
+                Console.WriteLine("What would you like to change?");
+                Console.WriteLine("(Name, Class, Race, Biography, Strength, Intelligence, Agility, Constitution, or Charisma)");
+                string choice = Console.ReadLine().Trim();
+                switch (choice.ToUpper())
+                {
+                    case "NAME": character.Name = ReadString("Enter your Character's name: ", true); return;
+                    case "CLASS": character.Class = ChooseClass(); return;
+                    case "RACE": character.Race = ChooseRace(); return;
+                    case "BIOGRAPHY": character.Biography = ReadString("Enter your Character's Biography (optional):", false); return;
+                    case "STRENGTH": character.Strength = ReadInt32("Enter your Character's Strength: ", 1, 100); return;
+                    case "INTELLIGENCE": character.Intelligence = ReadInt32("Enter your Character's Intelligence: ", 1, 100); return;
+                    case "AGILITY": character.Agility = ReadInt32("Enter your Character's Agility: ", 1, 100); return;
+                    case "CONSTITUTION":character.Constitution = ReadInt32("Enter your Character's Constitution: ", 1, 100); return;
+                    case "CHARISMA": character.Charisma = ReadInt32("Enter your Character's Charisma: ", 1, 100); return;
+                    default: DisplayError("Please enter a valid option"); break;
+                }
+            } while (true);
+        }
+
+        static void DeleteCharacter ()
+        {
+            if (character == null)
+                return;
+
+            if (!ReadBoolean("Are you sure you(Y/N)?"))
+                return;
+
+            character = null;
+        }
+
+        static bool ReadBoolean (string message)
+        {
+            Console.WriteLine(message);
+            do
+            {
+                string choice = Console.ReadLine().Trim().Substring(0, 1);
+
+                switch (choice.ToUpper())
+                {
+                    case "Y": return true;
+                    case "N": return false;
+                    default: DisplayError("Please enter either Y or N"); break;
+                }
+            } while (true);
+        }
+
+        static void DisplayError (string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static int ReadInt32 ( string message, int minimumValue, int maximumValue )
+        {
+            Console.Write(message);
+
+            do
+            {
+                string input = Console.ReadLine();
+
+                int result;
+                if (Int32.TryParse(input, out result) && result >= minimumValue && result <= maximumValue)
+                    return result;
+
+                DisplayError("The value must be an integral value >= " + minimumValue + " and <= " + maximumValue);
+            } while (true);
+        }
+
+        private static string ReadString ( string message, bool required )
+        {
+            Console.Write(message);
+
+            do
+            {
+                string input = Console.ReadLine().Trim();
+
+                if (!String.IsNullOrEmpty(input) || !required)
+                    return input;
+
+                DisplayError("Value is required");
+            } while (true);
+        }
+    }
+}
---
+++ labs/Lab2/JettSmith.CharacterCreator/Character.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace JettSmith.CharacterCreator
+{
+    public class Character
+    {
+        /// <summary>Gets or sets the name</summary>
+        public string Name
+        {
+            get { return (_name != null) ? _name : ""; }
+            set { _name = (value != null) ? value.Trim() : null; }
+        }
+
+        /// <summary>Gets or sets the class</summary>
+        public string Class
+        {
+            get { return (_class != null) ? _class : ""; }
+            set { _class = (value != null) ? value.Trim() : null; }
+        }
+
+        /// <summary>Gets or sets the race</summary>
+        public string Race
+        {
+            get { return (_race != null) ? _race : ""; }
+            set { _race = (value != null) ? value.Trim() : null; }
+        }
+
+        /// <summary>Gets or sets the biography</summary>
+        public string Biography
+        {
+            get { return (_biography != null) ? _biography : ""; }
+            set { _biography = (value != null) ? value.Trim() : null; }
+        }
+
+        /// <summary>Gets or sets the strength</summary>
+        public int Strength { get; set; }
+        /// <summary>Gets or sets the intelligence</summary>
+        public int Intelligence { get; set; }
+        /// <summary>Gets or sets the agility</summary>
+        public int Agility { get; set; }
+        /// <summary>Gets or sets the constitution</summary>
+        public int Constitution { get; set; }
+        /// <summary>Gets or sets the charisma</summary>
+        public int Charisma { get; set; }
+
+        private string _name;
+        private string _class;
+        private string _race;
+        private string _biography;
+
+        /// <summary>Validates the Character</summary>
+        /// <returns>An error if there is one</returns>
+        public string Validate ()
+        {
+            if (String.IsNullOrEmpty(Name))
+                return "Name is required";
+            if (String.IsNullOrEmpty(Class))
+                return "Class is required";
+            if (String.IsNullOrEmpty(Race))
+                return "Race is required";
+            if (Strength < 1 && Strength > 100)
+                return "Strength must be between 1 and 100";
+            if (Intelligence < 1 && Intelligence > 100)
+                return "Intelligence must be between 1 and 100";
+            if (Agility < 1 && Agility > 100)
+                return "Agility must be between 1 and 100";
+            if (Constitution < 1 && Constitution > 100)
+                return "Constitution must be between 1 and 100";
+            if (Charisma < 1 && Charisma > 100)
+                return "Charisma must be between 1 and 100";
+            return null;
+        }
+    }
+}
